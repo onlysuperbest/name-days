c9069e47a676064b16233d97b117a350c24d36a0
Developer <onlysuperbest@i.ua>
2021-06-22 16:15:04 +0300

    Improved ContentGenerator class

--- src/contentGenerator.php
+++ src/contentGenerator.php
@@ -2,14 +2,30 @@
 
 class ContentGenerator {
     public function generate($todaysNameDays) {
-        $generatedHTML = '<ul class="todays_name_days">';
+        $generatedHTML = '';
 
-        foreach ( $todaysNameDays as $singleNameDay ) {
-            $generatedHTML .= '<li>' . $singleNameDay . '</li>';
+        if ($this->arrayReadyToIterate($todaysNameDays)) {
+            $generatedHTML = '<ul class="todays_name_days">';
+
+            foreach ( $todaysNameDays as $singleNameDay ) {
+                $generatedHTML .= '<li>' . $singleNameDay . '</li>';
+            }
+
+            $generatedHTML .= '</ul>';
         }
 
-        $generatedHTML .= '</ul>';
-
         return $generatedHTML;
+    }
+
+    private function arrayReadyToIterate($todaysNameDays) {
+        if (is_array($todaysNameDays) ) {
+            return true;
+        }
+
+        if (count($todaysNameDays) > 0) {
+            return true;
+        }
+
+        return false;
     }
 }
